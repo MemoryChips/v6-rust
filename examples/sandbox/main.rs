1ccb64aee8af9bd9cf7d46863fac192c93b3c041
Rob Tamlyn <MemoryChips@yahoo.com>
2020-01-27 09:43:21 -0500

    Initial setup with v6 core and debug

---
+++ examples/sandbox/main.rs
@@ -0,0 +1,8 @@
+use v6::v6_core::App;
+
+fn main() {
+  let app = App {
+    app_name: "The app".to_string(),
+  };
+  app.run();
+}
---
+++ src/lib.rs
@@ -0,0 +1,30 @@
+pub mod v6_core {
+  // #[allow(dead_code)]
+  pub struct App {
+    pub app_name: String,
+  }
+  impl App {
+    // #[allow(dead_code)]
+    pub fn run(&self) {
+      println!("App name: {}", self.app_name)
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::v6_core::App;
+  #[test]
+  fn it_works() {
+    let _app = App {
+      app_name: "The app".to_string(),
+    };
+    _app.run();
+    assert_eq!(_app.app_name, "The app");
+    assert_eq!(2 + 2, 4);
+  }
+  // #[test]
+  // fn fail_test() {
+  //   panic!("Intentional Fail")
+  // }
+}
