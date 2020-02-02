bc15445c9ed064f3665cbc8d595e4a6b8a94e611
Rob Tamlyn <MemoryChips@yahoo.com>
2020-02-02 15:08:47 -0500

    Fixes sample test

--- tests/sample_test.rs
+++ tests/sample_test.rs
@@ -3,14 +3,15 @@ mod tests {
   use v6::v6_core::App;
   #[test]
   fn sample_test() {
+    let title = "English 日本語 русский язык 官話";
     let w_props = v6::v6_core::WindowProps {
-      title: "English 日本語 русский язык 官話".to_string(),
+      title: title.to_string(),
       w: 800,
       h: 600,
     };
     let mut _app = App::new(w_props, 1);
     _app.run();
-    assert_eq!(_app.app_name, "The app");
+    assert_eq!(_app.app_name, title);
     assert!(_app.is_running());
   }
   // #[test]
