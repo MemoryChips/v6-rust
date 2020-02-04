401b86873f35134725cfaf363778d4ea3f2e0448
Rob Tamlyn <MemoryChips@yahoo.com>
2020-02-04 13:28:12 -0500

    Adds more prep to windows.rs

--- src/window.rs
+++ src/window.rs
@@ -1,4 +1,4 @@
-// type WindowEventRcvr = std::sync::mpsc::Receiver<(f64, glfw::WindowEvent)>;
+type WindowEventRcvr = std::sync::mpsc::Receiver<(f64, glfw::WindowEvent)>;
 
 // trait
 
@@ -12,7 +12,9 @@ pub struct WindowProps {
 #[allow(dead_code)] // FIXME Remove when possible
 pub struct Window {
   props: WindowProps,
-  // event_callback: WindowEventRcvr,
+  window: glfw::Window,
+  events: WindowEventRcvr,
+  glfw: glfw::Glfw,
 }
 
 // fn glfw_error_callback(err: i32, description: &char) {
@@ -28,8 +30,26 @@ impl Window {
       "Creating window {0} ({1}, {2})",
       props.title, props.w, props.h
     );
-    let mut glfw: glfw::Glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Unable to initialize glfw"); // TODO: use OK
+    let (mut glfw, window, events) = Window::glfw_init(&props);
     glfw.set_error_callback(glfw::LOG_ERRORS);
-    Window { props }
+    Window {
+      props,
+      window,
+      events,
+      glfw,
+    }
+  }
+  fn glfw_init(props: &WindowProps) -> (glfw::Glfw, glfw::Window, WindowEventRcvr) {
+    let glfw: glfw::Glfw = glfw::init(glfw::FAIL_ON_ERRORS).expect("Unable to initialize glfw");
+    let (window, window_events) = glfw
+      .create_window(
+        props.w,
+        props.h,
+        &props.title,
+        // "English 日本語 русский язык 官話",
+        glfw::WindowMode::Windowed,
+      )
+      .expect("Unable to create window");
+    (glfw, window, window_events)
   }
 }
