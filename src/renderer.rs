e7f68ddcb96570b47a18e0e4e4905b745f413d04
Rob Tamlyn <MemoryChips@yahoo.com>
2020-02-05 12:47:42 -0500

    Adds api calls to renderer

--- src/renderer.rs
+++ src/renderer.rs
@@ -1,4 +1,4 @@
-use glam::*;
+use glam::{mat4, vec4, Mat4};
 
 #[allow(dead_code)]
 pub struct SceneData {
@@ -9,6 +9,30 @@ pub fn create_mat4_f(f: f32) -> Mat4 {
   let v = vec4(f, f, f, f);
   mat4(v, v, v, v)
 }
+
+pub mod api {
+  use glam::Vec4;
+  pub unsafe fn init() {
+    gl::Enable(gl::BLEND);
+    gl::BlendFunc(gl::SRC_ALPHA, gl::ONE_MINUS_SRC_ALPHA);
+    gl::Enable(gl::DEPTH_TEST);
+  }
+  pub unsafe fn set_viewport(x: i32, y: i32, w: i32, h: i32) {
+    gl::Viewport(x, y, w, h);
+  }
+  pub unsafe fn set_clear_color(color: &Vec4) {
+    gl::ClearColor(color.x(), color.y(), color.z(), color.w());
+  }
+  pub unsafe fn clear() {
+    gl::Clear(gl::COLOR_BUFFER_BIT | gl::DEPTH_BUFFER_BIT);
+  }
+}
+
+// void OpenGLRendererAPI::DrawIndexed(const Ref<VertexArray> &vertexArray) {
+// glDrawElements(GL_TRIANGLES, vertexArray->GetIndexBuffer()->GetCount(),
+// GL_UNSIGNED_INT, nullptr);
+// // glBindTexture(GL_TEXTURE_2D, 0); // unbind texture
+// }
 
 #[cfg(test)]
 mod tests {
