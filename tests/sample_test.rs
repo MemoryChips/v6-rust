d31fde24675037f3615b868713744a46b3147877
Rob Tamlyn <MemoryChips@yahoo.com>
2020-02-05 19:01:54 -0500

    Fixes sample test

--- tests/sample_test.rs
+++ tests/sample_test.rs
@@ -1,6 +1,5 @@
 #[cfg(test)]
 mod tests {
-  use v6::core::App;
   #[test]
   fn sample_test() {
     let title = "English 日本語 русский язык 官話";
@@ -9,7 +8,7 @@ mod tests {
       w: 800,
       h: 600,
     };
-    let mut app = App::new(title, w_props, 1);
+    let mut app = v6::App::new(title, w_props, 1);
     app.run();
     assert_eq!(app.app_name, title);
     assert!(app.is_running());
